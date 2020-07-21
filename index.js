7a1d154d5fa490d48b75cf08a426612462c260f0
Francis Bonacic <francisbonacic@icloud.com>
2020-07-21 13:15:23 -0700

    pushing through but still working on it...

--- index.js
+++ index.js
@@ -4,13 +4,41 @@ console.log(fifaData);
 console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+/* Task 1: Investigate the data above. Practice accessing 
+data by console.log-ing the following pieces of data 
 
 (a) Home Team name for 2014 world cup final
 (b) Away Team name for 2014 world cup final
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+
+
+
+
+console.log ()
+console.log ()
+console.log ()
+console.log ()
+console.log ()
+
+
+// function getData (array,)
+
+
+
+
+
+// const sampleArray = [1,2,3,4,5]
+
+
+
+// console.log (sampleArray[0]);
+// console.log (sampleArray.length)
+
+
+
+
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
